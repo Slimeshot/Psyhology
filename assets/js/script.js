81b77e6ecb415d34e4f25c37b7234a6bfce9881b
Evgeny Hohlov <slimeshot@mail.ru>
2021-03-16 23:46:00 +0300

    feat: add adaptiv

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
-    slidesPerView: 3,
-    spaceBetween: 30,
+    
     loop: true,
   
     navigation: {
@@ -10,4 +9,34 @@ const swiper = new Swiper('.swiper-container', {
       prevEl: '.swiper-button-prev',
     },
 
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      // when window width is >= 480px
+      980: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+      // when window width is >= 640px
+      
+    }
+
   });
+
+
+const menuIcon = document.querySelector('.header__burger');
+const menu = document.querySelector('.header__menu');
+
+
+menuIcon.addEventListener('click', () => {
+  menu.classList.toggle('change');
+  menuIcon.classList.toggle('btn-active');
+  
+})
