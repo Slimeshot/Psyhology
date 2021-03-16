fe806e3a360e8c074edbf4f95b8b673f856faf4d
Evgeny Hohlov <slimeshot@mail.ru>
2021-03-16 19:34:02 +0300

    feat: add new section

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
